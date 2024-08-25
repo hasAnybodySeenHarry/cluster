f10f6e61526f0d48dd216d810f9df3d9ee9837cc
harryd <harry@Wais-MacBook-Pro.local>
2024-08-25 14:03:46 +0700

    Added deployment controller constructs

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+
+kubectl create namespace argocd
+
+helm install argocd argo/argo-cd --namespace argocd
+
+kubectl apply -f argo/ingress-nginx.yaml
+
 helm uninstall my-infra
 helm uninstall expenses
 helm uninstall notifier
@@ -7,3 +16,5 @@ helm install my-infra application-infrastructure/
 helm install expenses expenses/
 helm install notifier notifier/
 helm install throttler throttler/
+
+kubectl apply -f ingress.yaml
