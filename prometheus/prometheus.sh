88308e35ba48f9b53ec2c91b33e0484487715a25
harryd <harry@Wais-MacBook-Pro.local>
2024-09-01 16:01:52 +0700

    Added prom chart

--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -3,6 +3,6 @@ helm repo update prometheus-community
 helm search repo prometheus-community
 
 helm install prometheus prometheus-community/kube-prometheus-stack \
---version "62.3.1" \ 
---namespace monitoring \ 
---create-namespace
+    --version "62.3.1" \
+    --namespace monitoring \
+    --create-namespace
