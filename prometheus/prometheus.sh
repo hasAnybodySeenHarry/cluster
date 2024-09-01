4fa063038669656867409f134aa6db1b656aa2c0
harryd <harry@Wais-MacBook-Pro.local>
2024-09-01 16:55:27 +0700

    Added prom adapter

--- prometheus/prometheus.sh
+++
@@ -1,8 +0,0 @@
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update prometheus-community
-helm search repo prometheus-community
-
-helm install prometheus prometheus-community/kube-prometheus-stack \
-    --version "62.3.1" \
-    --namespace monitoring \
-    --create-namespace
--- terra.sh
+++ terra.sh
@@ -1,4 +1,18 @@
-# argocd
+# prometheus (helm-provider)
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update prometheus-community
+helm search repo prometheus-community
+
+helm install prometheus prometheus-community/kube-prometheus-stack \
+    --version "62.3.1" \
+    --namespace monitoring \
+    --create-namespace
+
+# prometheus-adapter (helm-provider)
+helm upgrade --install prometheus-adapter prometheus-community/prometheus-adapter \
+    -f /adapter/values.yaml
+
+# argocd (helm-provider)
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 kubectl create namespace argocd
@@ -7,5 +21,5 @@ helm install argocd argo/argo-cd --namespace argocd
 # this will be later migrated to a controller
 helm install my-infra application-infrastructure/
 
-# environments (apps, ingress, networking rules)
+# environments: apps, ingress, networking policies (kubectl-provider)
 kubectl apply -f .init/
