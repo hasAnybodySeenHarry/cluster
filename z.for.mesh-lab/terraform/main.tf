8535c0d1f5bc46809ca9e8b5dde1e139887fa3bc
Harry Yan <noreply@harryyan.com>
2025-07-18 01:54:50 +0100

    Added manifests for mesh lab

---
+++ z.for.mesh-lab/terraform/main.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.16.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubectl" {
+  load_config_file = true
+  config_path      = "~/.kube/config"
+}
+
+locals {
+  argocd_apps = {
+    for app in var.argocd_apps : app.app-name => app
+  }
+}
+
+// nginx helm release
+resource "helm_release" "nginx" {
+  namespace        = "ingress-nginx"
+  create_namespace = true
+
+  name  = "ingress-nginx"
+  chart = "ingress-nginx"
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+
+  values = [
+    yamlencode({
+      controller = {
+        allowSnippetAnnotations = "true"
+        service = {
+          type = "NodePort"
+        }
+        podAnnotations = {
+          "linkerd.io/inject" = "enabled"
+        }
+        config = {
+          annotations-risk-level = "Critical"
+        }
+      }
+    })
+  ]
+}
+
+// argocd helm release
+resource "helm_release" "argocd" {
+  namespace        = "argocd"
+  create_namespace = true
+
+  name  = "argocd"
+  chart = "argo-cd"
+
+  repository = "https://argoproj.github.io/argo-helm"
+}
+
+resource "kubectl_manifest" "argocd_applications" {
+  for_each = local.argocd_apps
+
+  yaml_body = templatefile("${path.module}/application.yaml.tmpl", {
+    app-name        = each.value.app-name
+    repository-url  = each.value.repository-url
+    target-revision = each.value.target-revision
+    source-path     = each.value.source-path
+    server-url      = each.value.server-url
+  })
+
+  depends_on = [
+    helm_release.argocd
+  ]
+}
---
+++ z.for.mesh-lab/terraform/terraform.tfvars
@@ -0,0 +1,9 @@
+argocd_apps = [
+  {
+    app-name        = "dev-apps",
+    repository-url  = "https://github.com/hasAnybodySeenHarry/cluster",
+    target-revision = "HEAD",
+    source-path     = "z.for.mesh-lab"
+    server-url      = "https://kubernetes.default.svc"
+  },
+]
