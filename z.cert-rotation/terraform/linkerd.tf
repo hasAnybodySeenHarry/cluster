d470fee05442a2c0102836327c1c4e0b12d14a8f
Harry Yan <noreply@harryyan.com>
2025-07-30 21:50:28 +0100

    Added rbac resourcea

--- z.cert-rotation/terraform/linkerd.tf
+++ z.cert-rotation/terraform/linkerd.tf
@@ -1,11 +1,16 @@
+locals {
+  linkerd_ns      = "linkerd"
+  cert_manager_ns = "cert-manager"
+}
+
 resource "kubernetes_namespace" "linkerd" {
   metadata {
-    name = "linkerd"
+    name = local.linkerd_ns
   }
 }
 
 resource "helm_release" "cert_manager" {
-  namespace        = "cert-manager"
+  namespace        = local.cert_manager_ns
   create_namespace = true
 
   name       = "cert-manager"
@@ -20,7 +25,7 @@ resource "helm_release" "cert_manager" {
 }
 
 resource "helm_release" "trust-manager" {
-  namespace        = "cert-manager"
+  namespace        = local.cert_manager_ns
   create_namespace = false
 
   name       = "trust-manager"
@@ -32,7 +37,7 @@ resource "helm_release" "trust-manager" {
 
   set {
     name  = "app.trust.namespace"
-    value = "cert-manager"
+    value = local.cert_manager_ns
   }
 
   depends_on = [
@@ -40,3 +45,34 @@ resource "helm_release" "trust-manager" {
   ]
 }
 
+resource "kubernetes_role" "cert-manager-secret-creator" {
+  metadata {
+    name      = "cert-manager-secret-creator"
+    namespace = local.linkerd_ns
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["secrets"]
+    verbs      = ["create", "get", "update", "patch"]
+  }
+}
+
+resource "kubernetes_role_binding" "cert-manager-secret-creator-binding" {
+  metadata {
+    name = "cert-manager-secret-creator-binding"
+    namespace = local.linkerd_ns
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = "cert-manager-secret-creator"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "cert-manager"
+    namespace = local.cert_manager_ns
+  }
+}
