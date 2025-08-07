2500d0f78d494664e80b5b4d43c57e093eeac376
Harry Yan <noreply@harryyan.com>
2025-08-07 08:06:55 +0100

    Added viz components

---
+++ z.for.viz/terraform/linkerd-viz.tf
@@ -0,0 +1,26 @@
+resource "helm_release" "linkerd" {
+  name             = "linkerd-viz"
+  namespace        = "linkerd-viz"
+  repository       = "https://helm.linkerd.io/edge"
+  chart            = "linkerd-viz"
+  version          = "2025.8.1"
+  create_namespace = true
+
+  atomic = true
+
+  values = [ 
+    <<-EOT
+      linkerdVersion: "edge-25.8.1"
+      tapInjector:
+        replicas: 0
+      dashboard:
+        replicas: 0
+      tap:
+        replicas: 0
+    EOT
+   ]
+
+  depends_on = [
+    helm_release.linkerd
+  ]
+}
