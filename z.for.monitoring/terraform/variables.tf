a0ebb7c12fefa7c2df6127c1a56e27a85d870477
Harry Yan <noreply@harryyan.com>
2025-08-22 14:09:35 +0100

    Adjusted releases count

--- z.for.monitoring/terraform/variables.tf
+++ z.for.monitoring/terraform/variables.tf
@@ -4,3 +4,23 @@ variable "inject_mesh" {
   description = "Whether to inject data plane proxies into prometheus components"
 }
 
+# controlplane:~$ cat nodeexporterrules.yaml 
+# apiVersion: monitoring.coreos.com/v1
+# kind: PrometheusRule
+# metadata:
+#   name: my-nodeexporter-rules
+#   namespace: monitoring
+#   labels:
+#     release: prometheus
+# spec:
+#   groups:
+#   - name: node-exporter
+#     rules:
+#     - alert: NodeExporterDown
+#       expr: up{job="node-exporter"} == 0
+#       for: 1m
+#       labels:
+#         severity: critical
+#       annotations:
+#         summary: "Node Exporter is down"
+#         description: "Prometheus target on {{ $labels.instance }} unreachable >1m."
